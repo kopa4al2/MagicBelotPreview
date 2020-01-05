045d12d2c1efc19c3a57338a0f4fb939168c5e9f
unknown <a582hs@gmail.com>
2020-01-05 12:08:39 +0200

    Updates

---
+++ precache-manifest.3b9d3e6858451fbdbb99001c23229bc6.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d243e42ff8b5b94953b91ad28b857f7e",
+    "url": "/MagicBelotPreview/index.html"
+  },
+  {
+    "revision": "7c0eae1b0a31ec9cfdf1",
+    "url": "/MagicBelotPreview/static/css/2.a4e9ec4a.chunk.css"
+  },
+  {
+    "revision": "34070da2c51b12983472",
+    "url": "/MagicBelotPreview/static/css/main.9aae686c.chunk.css"
+  },
+  {
+    "revision": "7c0eae1b0a31ec9cfdf1",
+    "url": "/MagicBelotPreview/static/js/2.aa7d15a6.chunk.js"
+  },
+  {
+    "revision": "798cebdabb9ec58276ef3ac7a0afbf66",
+    "url": "/MagicBelotPreview/static/js/2.aa7d15a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34070da2c51b12983472",
+    "url": "/MagicBelotPreview/static/js/main.acf6ae38.chunk.js"
+  },
+  {
+    "revision": "e5eb70d25251c517fc43",
+    "url": "/MagicBelotPreview/static/js/runtime-main.c80fd0b6.js"
+  },
+  {
+    "revision": "62a0d996d0d7209f20bb11062eae74d7",
+    "url": "/MagicBelotPreview/static/media/10-CLUBS.62a0d996.png"
+  },
+  {
+    "revision": "5c9889bb2d87d5fbf8d98bbbde14ce1f",
+    "url": "/MagicBelotPreview/static/media/10-DIAMONDS.5c9889bb.png"
+  },
+  {
+    "revision": "d639a977276ef4dae4170240da43dc59",
+    "url": "/MagicBelotPreview/static/media/10-HEARTS.d639a977.png"
+  },
+  {
+    "revision": "50ce127014557b3755dbdc55a4d563af",
+    "url": "/MagicBelotPreview/static/media/10-SPADES.50ce1270.png"
+  },
+  {
+    "revision": "1d5066c467bbcb5af1cc9c044630b48e",
+    "url": "/MagicBelotPreview/static/media/2X.1d5066c4.svg"
+  },
+  {
+    "revision": "9860e56f7940a0e401d48ee071910742",
+    "url": "/MagicBelotPreview/static/media/4X.9860e56f.svg"
+  },
+  {
+    "revision": "97e6d1f6b0162bbab6c473fd62b38a3a",
+    "url": "/MagicBelotPreview/static/media/7-CLUBS.97e6d1f6.png"
+  },
+  {
+    "revision": "733cd116e284b519270f4eb1dfd111ea",
+    "url": "/MagicBelotPreview/static/media/7-DIAMONDS.733cd116.png"
+  },
+  {
+    "revision": "764a674894e011abc4c92eb219a7120b",
+    "url": "/MagicBelotPreview/static/media/7-HEARTS.764a6748.png"
+  },
+  {
+    "revision": "c44c869818195c3ed0f4b18284741a74",
+    "url": "/MagicBelotPreview/static/media/7-SPADES.c44c8698.png"
+  },
+  {
+    "revision": "65e912abb788c86cdd19df992ee835d7",
+    "url": "/MagicBelotPreview/static/media/8-CLUBS.65e912ab.png"
+  },
+  {
+    "revision": "b0aa094e8efe02e4c2b80706bd553dc8",
+    "url": "/MagicBelotPreview/static/media/8-DIAMONDS.b0aa094e.png"
+  },
+  {
+    "revision": "01de1ab939bc4278ebd9049ec1ed32f1",
+    "url": "/MagicBelotPreview/static/media/8-HEARTS.01de1ab9.png"
+  },
+  {
+    "revision": "c5349a72057033d1526581954a879766",
+    "url": "/MagicBelotPreview/static/media/8-SPADES.c5349a72.png"
+  },
+  {
+    "revision": "a11d2254b05a3fd6d37f73d72b04d639",
+    "url": "/MagicBelotPreview/static/media/9-CLUBS.a11d2254.png"
+  },
+  {
+    "revision": "90e20d832b26a8548965702cafcfdda3",
+    "url": "/MagicBelotPreview/static/media/9-DIAMONDS.90e20d83.png"
+  },
+  {
+    "revision": "f73cef8b051dd395c48db5aaa4c1700d",
+    "url": "/MagicBelotPreview/static/media/9-HEARTS.f73cef8b.png"
+  },
+  {
+    "revision": "27a49aa2e7660d751283d62bb2bd33c2",
+    "url": "/MagicBelotPreview/static/media/9-SPADES.27a49aa2.png"
+  },
+  {
+    "revision": "6e9de0ecee0e7e31922a05265ece3e5b",
+    "url": "/MagicBelotPreview/static/media/A-SPADES.6e9de0ec.png"
+  },
+  {
+    "revision": "882d4ff569a5b8ce0d88e92ed2dd791b",
+    "url": "/MagicBelotPreview/static/media/J-CLUBS.882d4ff5.png"
+  },
+  {
+    "revision": "3dfc401bcbaea6fa539d31c8b301c19b",
+    "url": "/MagicBelotPreview/static/media/J-DIAMONDS.3dfc401b.png"
+  },
+  {
+    "revision": "d8f39043a4be72e450ad72d9c43d6e0b",
+    "url": "/MagicBelotPreview/static/media/J-HEARTS.d8f39043.png"
+  },
+  {
+    "revision": "889eaf3b8a5dba3863f70378922f33ab",
+    "url": "/MagicBelotPreview/static/media/J-SPADES.889eaf3b.png"
+  },
+  {
+    "revision": "6623a8dcb656041d5c4123b7f6aa5459",
+    "url": "/MagicBelotPreview/static/media/K-CLUBS.6623a8dc.png"
+  },
+  {
+    "revision": "10d97590b745ff3b9abcca5be7fd572f",
+    "url": "/MagicBelotPreview/static/media/K-DIAMONDS.10d97590.png"
+  },
+  {
+    "revision": "e3a8265ce0c86a7521ac1f70b627a1f3",
+    "url": "/MagicBelotPreview/static/media/K-HEARTS.e3a8265c.png"
+  },
+  {
+    "revision": "a76231386b27a8f434fe0af0dfa725da",
+    "url": "/MagicBelotPreview/static/media/K-SPADES.a7623138.png"
+  },
+  {
+    "revision": "b3c1faccc6fea64e22b394cf2a4ccaa2",
+    "url": "/MagicBelotPreview/static/media/Q-CLUBS.b3c1facc.png"
+  },
+  {
+    "revision": "29a49e8931187bb1a5b73ad22604aece",
+    "url": "/MagicBelotPreview/static/media/Q-DIAMONDS.29a49e89.png"
+  },
+  {
+    "revision": "d5e23e8545fe91b174b19eb8754dfc19",
+    "url": "/MagicBelotPreview/static/media/Q-HEARTS.d5e23e85.png"
+  },
+  {
+    "revision": "d53eb9fdee7e00bf7360a6178e8cb38f",
+    "url": "/MagicBelotPreview/static/media/Q-SPADES.d53eb9fd.png"
+  },
+  {
+    "revision": "5f78575f9ecd7ff2efdfff367ced1a30",
+    "url": "/MagicBelotPreview/static/media/blizzard_storm.5f78575f.jpeg"
+  },
+  {
+    "revision": "e91e06456b832d5980d1b71f86b9a2a2",
+    "url": "/MagicBelotPreview/static/media/card-back.e91e0645.png"
+  },
+  {
+    "revision": "9dfe94c643cea336794689af7b0b117d",
+    "url": "/MagicBelotPreview/static/media/destiny-creator.9dfe94c6.jpg"
+  },
+  {
+    "revision": "1854c4f106f70c9d6a6d6633e1f5c86c",
+    "url": "/MagicBelotPreview/static/media/loading-img.1854c4f1.png"
+  },
+  {
+    "revision": "22c90887a5d6d7bae368250b95c1ce20",
+    "url": "/MagicBelotPreview/static/media/material-logo.22c90887.svg"
+  },
+  {
+    "revision": "55373cad76de2b1c920959540e55696f",
+    "url": "/MagicBelotPreview/static/media/react-logo.55373cad.png"
+  },
+  {
+    "revision": "9b0986e777dcb4d30c69b8556855060b",
+    "url": "/MagicBelotPreview/static/media/react-router-logo.9b0986e7.png"
+  },
+  {
+    "revision": "e0be2b2cb491034e68526b71eeb7a808",
+    "url": "/MagicBelotPreview/static/media/redux-logo.e0be2b2c.png"
+  },
+  {
+    "revision": "6d94f33ca2440979e0ba969184403335",
+    "url": "/MagicBelotPreview/static/media/styled-components-logo.6d94f33c.png"
+  }
+]);
